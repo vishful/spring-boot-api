4ade8607fa5348c21158013576640dbfe47341ee
Vishnu R Pillai <vishnurpillai9914@gmail.com>
2021-08-29 21:17:19 +0530

    Cross Origin

--- src/main/java/com/example/controller/CourseController.java
+++ src/main/java/com/example/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,8 @@ import com.example.model.Course;
 
 import com.example.service.CourseService;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CourseController {
 
